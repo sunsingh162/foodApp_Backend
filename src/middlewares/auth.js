384dda926a0d06c8dffd5a84ec7a82fbf5871587
Sunny Kumar Singh <srajukumarsingh@ivycomptech.com>
2024-11-21 17:11:36 +0530

    added restaurant schema and router for restaurant to add and and show list and made auth middleware for user authentication

---
+++ src/middlewares/auth.js
@@ -0,0 +1,23 @@
+const userModel = require("../models/user");
+const jwt = require("jsonwebtoken");
+
+const userAuth = async (req, res, next) => {
+  try {
+    const { token } = req.cookies;
+    if (!token) {
+      return res.status(401).send("Please Login!!!");
+    }
+    const decodedMessage = await jwt.verify(token, "FoodApp@6798");
+    const { _id } = decodedMessage;
+    const user = await userModel.findById({ _id });
+    if (!user) {
+      throw new Error("User not found");
+    }
+    req.user = user;
+    next();
+  } catch (err) {
+    res.status(400).send("ERROR : " + err.message);
+  }
+};
+
+module.exports = { userAuth };
--- src/routes/restaurant.js
+++ src/routes/restaurant.js
@@ -1,29 +1,36 @@
-const express = require('express');
-const Restaurant = require('../models/restaurant');
+const express = require("express");
+const Restaurant = require("../models/restaurant");
+const { userAuth } = require("../middlewares/auth");
 
 const restaurantRouter = express.Router();
 
-// API to add a restaurant with categories and items
-restaurantRouter.post('/addResItems', async (req, res) => {
-  const { restaurantName, categories } = req.body;
-
-  if (!restaurantName || !categories || !Array.isArray(categories)) {
-    return res.status(400).json({ message: 'Invalid input data.' });
-  }
-
+// API to add multiple restaurants
+restaurantRouter.post("/addRes", async (req, res) => {
   try {
-    const restaurant = new Restaurant({
-      restaurantName,
-      categories,
-    });
-
-    await restaurant.save();
-    res.status(200).json({ message: 'Restaurant data added successfully!', restaurant });
+    const restaurants = req.body;
+    if (!restaurants || !Array.isArray(restaurants)) {
+      return res
+        .status(400)
+        .json({ message: "Please provide restaurants" });
+    }
+    const restaurantDocuments = restaurants.map(
+      (restaurant) => new Restaurant(restaurant)
+    );
+    const savedRestaurants = await Restaurant.insertMany(restaurantDocuments);
+    res.status(201).json(savedRestaurants);
   } catch (error) {
-    console.error('Error saving restaurant data:', error);
-    res.status(500).json({ message: 'Server error' });
+    res.status(500).json({ message: error.message });
   }
 });
 
+//API to get all the restaurant when home page loads
+restaurantRouter.get("/getRes", userAuth, async (req, res) => {
+  try {
+    const restaurants = await Restaurant.find();
+    res.json(restaurants);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 
-module.exports = restaurantRouter
+module.exports = restaurantRouter;
